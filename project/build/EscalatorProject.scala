756d879d29bcba06f66a190a146868e7d88b2dad
Brian Howard <bhoward@depauw.edu>
2010-08-14 19:58:37 -0400

    Added Rhino dependency for SBT build

--- project/build/EscalatorProject.scala
+++ project/build/EscalatorProject.scala
@@ -2,9 +2,10 @@ import sbt._
 
 class EscalatorProject(info: ProjectInfo) extends DefaultProject(info) {
   val swing = "org.scala-lang" % "scala-swing" % "2.8.0"
-//  val compiler = "org.scala-lang" % "scala-compiler" % "2.8.0"
+  val compiler = "org.scala-lang" % "scala-compiler" % "2.8.0"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
   val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.7"
+  val rhino = "rhino" % "js" % "1.7R2"
   
   override def compileOptions = super.compileOptions ++ Seq(target(Target.Java1_5))
   
