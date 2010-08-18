90aa8a7e7693bf781c7dd326fdd4d5ac58da2005
Brian Howard <bhoward@depauw.edu>
2010-08-18 12:12:04 -0400

    More TODO, and fixed the scripts to match the distribution layout

--- distrib/esc-gui.bat
+++ distrib/esc-gui.bat
@@ -6,4 +6,4 @@ SET ESCHOME=.
 SET ESCLIB=%ESCHOME%\lib
 
 REM Should not need to touch this
-%JAVAEXE% -cp %ESCHOME%\escalator.jar;%ESCLIB%\js.jar;%ESCLIB%\scala-compiler.jar;%ESCLIB%\scala-library.jar;%ESCLIB%\scala-swing.jar;%ESCLIB%\scalacheck.jar;%ESCLIB%\specs.jar edu.depauw.escalator.GUIMain %*
+%JAVAEXE% -cp %ESCLIB%\escalator.jar;%ESCLIB%\js.jar;%ESCLIB%\scala-compiler.jar;%ESCLIB%\scala-library.jar;%ESCLIB%\scala-swing.jar;%ESCLIB%\scalacheck.jar;%ESCLIB%\specs.jar edu.depauw.escalator.GUIMain %*
