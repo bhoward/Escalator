3e4d4bbbd64211d7ecc10e4148c965ee1a6c8dae
Brian Howard <bhoward@depauw.edu>
2010-08-30 17:18:27 -0400

    Better fix for the .bat file

--- src/main/resources/escalator.bat
+++ src/main/resources/escalator.bat
@@ -1,11 +1,8 @@
 @echo off
 
-
 REM Change these as necessary
-
 SET JAVAEXE=java.exe
-
+SET ESCHOME=.
 
 REM Should not need to touch this
-
-%JAVAEXE% -cp .\escalator_2.8.0-2.0.jar;.\js-1.7R2.jar;.\scala-compiler.jar;.\scala-library.jar;.\scala-swing-2.8.0.jar;.\scalacheck_2.8.0-1.7.jar;.\specs_2.8.0-1.6.5.jar;.\jlfgr-1_0.jar;. edu.depauw.escalator.Main %*
+%JAVAEXE% -cp %ESCHOME%\escalator_2.8.0-2.0.jar;%ESCHOME%\js-1.7R2.jar;%ESCHOME%\scala-compiler.jar;%ESCHOME%\scala-library.jar;%ESCHOME%\scala-swing-2.8.0.jar;%ESCHOME%\scalacheck_2.8.0-1.7.jar;%ESCHOME%\specs_2.8.0-1.6.5.jar;%ESCHOME%\jlfgr-1_0.jar;%ESCHOME%\test-interface-0.3.jar;%ESCHOME% edu.depauw.escalator.Main %*
