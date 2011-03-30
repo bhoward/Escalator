1a89e4c7a4f56cc94812655c5fd230467a93345a
Brian Howard <bhoward@depauw.edu>
2011-03-30 11:57:38 -0400

    Updated to 2.8.1

--- project/build/EscalatorProject.scala
+++ project/build/EscalatorProject.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 class EscalatorProject(info: ProjectInfo) extends DefaultProject(info) {
-  val swing = "org.scala-lang" % "scala-swing" % "2.8.0"
-  val compiler = "org.scala-lang" % "scala-compiler" % "2.8.0"
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
-  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.7"
+  val swing = "org.scala-lang" % "scala-swing" % "2.8.1"
+  val compiler = "org.scala-lang" % "scala-compiler" % "2.8.1"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.7"
+  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.8"
   val rhino = "rhino" % "js" % "1.7R2"
 
   override def compileClasspath = super.compileClasspath +++ extraCompileClasspath
--- src/main/resources/escalator.bat
+++ src/main/resources/escalator.bat
@@ -5,4 +5,4 @@ SET JAVAEXE=java.exe
 SET ESCHOME=.
 
 REM Should not need to touch this
-%JAVAEXE% -cp %ESCHOME%\escalator_2.8.0-2.0.jar;%ESCHOME%\js-1.7R2.jar;%ESCHOME%\scala-compiler.jar;%ESCHOME%\scala-library.jar;%ESCHOME%\scala-swing-2.8.0.jar;%ESCHOME%\scalacheck_2.8.0-1.7.jar;%ESCHOME%\specs_2.8.0-1.6.5.jar;%ESCHOME%\jlfgr-1_0.jar;%ESCHOME%\test-interface-0.3.jar;%ESCHOME% edu.depauw.escalator.Main %*
+%JAVAEXE% -cp %ESCHOME%\escalator_2.8.1-2.0.jar;%ESCHOME%\js-1.7R2.jar;%ESCHOME%\scala-compiler.jar;%ESCHOME%\scala-library.jar;%ESCHOME%\scala-swing-2.8.1.jar;%ESCHOME%\scalacheck_2.8.1-1.8.jar;%ESCHOME%\specs_2.8.1-1.6.7.jar;%ESCHOME%\jlfgr-1_0.jar;%ESCHOME%\test-interface-0.5.jar;%ESCHOME% edu.depauw.escalator.Main %*
